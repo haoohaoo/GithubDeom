3e8c1fc29c470f3f1daaae2981d3dcc0a9c8634d
RaizelLee <33753236+RaizelLee@users.noreply.github.com>
2018-05-11 15:51:03 +0800

    ^^

--- ChatRoom01/Server.py
+++ ChatRoom01/Server.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
 import socket
 import threading
+import time
 from time import gmtime, strftime
-
+a = 0
 class Server:
     def __init__(self, host, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,15 +16,11 @@ class Server:
     def checkConnection(self):
         connection, addr = self.sock.accept()
         print('Accept a new connection', connection.getsockname(), connection.fileno())
-
         try:
             buf = connection.recv(1024).decode()
             if buf == '1':
                 # start a thread for new connection
-                connection.send(b'Welcome to chat room!\nPleace input your nick name:')
-                nickname = connection.recv(1024).decode()
-                connection.send(b'Now lets chat'+nickname)
-                mythread = threading.Thread(target=self.subThreadIn, args=(connection, connection.fileno(),nickname))
+                mythread = threading.Thread(target=self.subThreadIn, args=(connection, connection.fileno()))
                 mythread.setDaemon(True)
                 mythread.start()
 
@@ -37,14 +34,31 @@ class Server:
     def tellOthers(self, exceptNum, whatToSay,nickname):
         for c in self.mylist:
             if c.fileno() != exceptNum:
-                massage = (nickname+ ":" +whatToSay);
+                st = time.localtime(time.time())
+                times = time.strftime('[%H:%M:%S]', st)
+                massage = (nickname+ ":" +whatToSay + "     " + times)
                 try:
                     c.send(massage.encode())
                 except:
                     pass
 
-    def subThreadIn(self, myconnection, connNumber,nickname):
+    def subThreadIn(self, myconnection, connNumber):
         self.mylist.append(myconnection)
+        myconnection.send(b'Welcome to chat room!\nPleace input your nick name:')
+        nickname = myconnection.recv(1024).decode()
+        massage = ("Now lets chat, " +nickname)
+        myconnection.send(massage.encode())
+        massage1 = ("SYSTEM: "+nickname+ " in the chat room")
+        global a
+        a+=1
+        for c in self.mylist:
+            if c.fileno() != connNumber:
+                try:
+                    c.send(massage1.encode())
+                except:
+                    pass
+                massage2 = ("SYSTEM: " + str(a) + " people in the chat room")
+                c.send(massage2.encode())
         while True:
             try:
                 recvedMsg = myconnection.recv(1024).decode()
